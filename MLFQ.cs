00c66450001b21604fc91d55ca1c933be4955e41
Ehsan Khan <121447980+ehsankhan2293@users.noreply.github.com>
2025-04-27 16:35:31 +0530

    Code created from scratch for SRTF and MLFQ

---
+++ MLFQ.cs
@@ -0,0 +1,206 @@
+﻿using System;
+
+class Process
+{
+    public int Id;
+    public int ArrivalTime;
+    public int BurstTime;
+    public int WaitingTime;
+    public int TurnAroundTime;
+    public int ResponseTime;
+    public int CompletionTime;
+}
+
+class MLFQ
+{
+    static int MAX_PROCESS = 1000;
+    //Three queues are used, each hold upto 1000 process 
+    static Process[] Queue1 = new Process[MAX_PROCESS];
+    static Process[] Queue2 = new Process[MAX_PROCESS];
+    static Process[] Queue3 = new Process[MAX_PROCESS]; 
+    static int totalProcess;
+
+    // This function is used to sort the process based on its arrival time
+    static void SortProcess()
+    {
+        Process temp;
+        for (int i = 0; i < totalProcess; i++)
+        {
+            for (int j = i + 1; j < totalProcess; j++)
+            {
+                if (Queue1[i].ArrivalTime > Queue1[j].ArrivalTime)
+                {
+                    temp = Queue1[i];
+                    Queue1[i] = Queue1[j];
+                    Queue1[j] = temp;
+                }
+            }
+        }
+    }
+
+    //DRIVER CODE
+    static void Main()
+    {
+        int k = 0;
+        int round = 0;
+        int current_time = 0;
+        int time_quantum_1 = 5;
+        int time_quantum_2 = 8;
+        int all_process_time = 0;
+        int flag = 0;
+        int avg_waiting_time = 0;
+        int avg_turn_time = 0;
+        int c=1;
+        List<Process> gantt_chart_process = new List<Process>();
+
+
+        Console.Write("Welcome To Multi Level Feedback Queue Scheduling\n");
+        Console.Write("Please enter number of process: ");
+        var input = Console.ReadLine();
+        if (input != null)
+            totalProcess = int.Parse(input);
+
+        for (int i = 0; i < totalProcess; i++)
+        {
+            Queue1[i] = new Process();
+            Queue2[i] = new Process();
+            Queue3[i] = new Process();
+        }
+
+        for (int i = 0; i < totalProcess; i++, c++)
+        {
+            Queue1[i].Id = c;
+            Console.Write($"\nPlease enter arrival time and burst time of process {Queue1[i].Id}: ");
+            var @params = Console.ReadLine().Split();
+            Queue1[i].ArrivalTime = int.Parse(@params[0]);
+            Queue1[i].BurstTime = int.Parse(@params[1]);
+            Queue1[i].ResponseTime = Queue1[i].BurstTime;
+            all_process_time += Queue1[i].BurstTime;
+        }
+
+        SortProcess();
+        Console.WriteLine("========================================================");
+        current_time = Queue1[0].ArrivalTime;
+        Console.WriteLine("Process status in first queue with time quantum=5");
+        Console.WriteLine("Process\t\tRT\t\tWT\t\tTAT\t\t");
+        int found = 0;
+        for (int i = 0; i < totalProcess; i++)
+        {
+            
+            if (Queue1[i].ResponseTime <= time_quantum_1)
+            {
+                gantt_chart_process.Add(Queue1[i]);
+                current_time += Queue1[i].ResponseTime; 
+                Queue1[i].ResponseTime = 0;
+                Queue1[i].WaitingTime = current_time - Queue1[i].ArrivalTime - Queue1[i].BurstTime; 
+                Queue1[i].TurnAroundTime = current_time - Queue1[i].ArrivalTime; 
+                Console.WriteLine($"{Queue1[i].Id}\t\t{Queue1[i].BurstTime}\t\t{Queue1[i].WaitingTime}\t\t{Queue1[i].TurnAroundTime}");
+                avg_waiting_time += Queue1[i].WaitingTime;
+                avg_turn_time += Queue1[i].TurnAroundTime;
+                found = 1;
+            }
+            else 
+            {
+                gantt_chart_process.Add(Queue2[k]);
+                Queue2[k].WaitingTime = current_time;
+                current_time += time_quantum_1;
+                Queue1[i].ResponseTime -= time_quantum_1;
+                Queue2[k].BurstTime = Queue1[i].ResponseTime;
+                Queue2[k].ResponseTime = Queue2[k].BurstTime;
+                Queue2[k].Id = Queue1[i].Id;
+                k++;
+                flag = 1;
+            }
+        }
+
+        if (found == 0)
+        {
+            Console.WriteLine("\n No process found\n");
+        }
+        Console.WriteLine("========================================================");
+        if (flag == 1)
+        {
+            Console.WriteLine("Process status in second queue with time quantum=8");
+            Console.WriteLine("Process\t\tRT\t\tWT\t\tTAT\t\t");
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            if (Queue2[i].ResponseTime <= time_quantum_2)
+            {
+                gantt_chart_process.Add(Queue2[i]);
+                current_time += Queue2[i].ResponseTime; 
+                Queue2[i].ResponseTime = 0;
+                Queue2[i].WaitingTime = current_time - time_quantum_1 - Queue2[i].BurstTime; 
+                Queue2[i].TurnAroundTime = current_time - Queue2[i].ArrivalTime; 
+                Console.WriteLine($"{Queue2[i].Id}\t\t{Queue2[i].BurstTime}\t\t{Queue2[i].WaitingTime}\t\t{Queue2[i].TurnAroundTime}");
+                avg_waiting_time += Queue2[i].WaitingTime;
+                avg_turn_time += Queue2[i].TurnAroundTime;
+            }
+            else 
+            {
+                gantt_chart_process.Add(Queue3[round]);
+                Queue3[round].ArrivalTime = current_time;
+                current_time += time_quantum_2;
+                Queue2[i].ResponseTime -= time_quantum_2;
+                Queue3[round].BurstTime = Queue2[i].ResponseTime;
+                Queue3[round].ResponseTime = Queue3[round].BurstTime;
+                Queue3[round].Id = Queue2[i].Id;
+                round++;
+                flag = 2;
+            }
+        }
+        Console.WriteLine("========================================================");
+        if (flag == 2)
+            Console.WriteLine("\nProcess status third queue using FCFS ");
+
+        for (int i = 0; i < round; i++)
+        {
+            if (i == 0)
+                Queue3[i].CompletionTime = Queue3[i].BurstTime + current_time - time_quantum_1 - time_quantum_2;
+            else
+                Queue3[i].CompletionTime = Queue3[i - 1].CompletionTime + Queue3[i].BurstTime;
+        }
+
+        for (int i = 0; i < round; i++)
+        {
+            Queue3[i].TurnAroundTime = Queue3[i].CompletionTime;
+            Queue3[i].WaitingTime = Queue3[i].TurnAroundTime - Queue3[i].BurstTime;
+            Console.WriteLine($"{Queue3[i].Id}\t\t{Queue3[i].BurstTime}\t\t{Queue3[i].WaitingTime}\t\t{Queue3[i].TurnAroundTime}\t\t");
+
+            avg_waiting_time += Queue3[i].WaitingTime;
+            avg_turn_time += Queue3[i].TurnAroundTime;
+        }
+    //      Console.WriteLine("========================================================");
+    //displayGanttChart(gantt_chart_process);
+        Console.WriteLine("========================================================");
+        Console.WriteLine("CUrrent time " + current_time
+            );
+        float cpu = ((float)all_process_time / (float)current_time) * 100;
+        Console.WriteLine("Average Waiting Time :" + avg_waiting_time / totalProcess);
+        Console.WriteLine("Average Turn Around Time :" + avg_turn_time / totalProcess);
+        Console.WriteLine("Throughput process :" + current_time / totalProcess);
+        Console.WriteLine("CPU Utilization  :" + cpu + "%");
+
+    }
+
+    private static void displayGanttChart(List<Process> processList)
+    {
+        Console.WriteLine("\nGantt Chart:");
+        foreach (var proc in processList)
+        {
+            Console.Write($"P{proc.Id} ");
+        }
+        Console.WriteLine();
+
+        for (int id = 0; id <= processList.Count; id++)
+        {
+            if (id < 10)
+                Console.Write(id == 0 ? "0  " : $"{id}  ");
+            else
+                Console.Write(id == 0 ? "0  " : $"{id} ");
+
+        }
+        Console.WriteLine();
+    }
+}
---
+++ SRTF.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Process
+{
+    public int Process_id { get; set; }
+    public int TurnaroundTime { get; set; }
+    public int WaitingTime { get; set; }
+
+    public int ArrivalTime { get; set; }
+    public int BurstTime { get; set; }
+
+    public int RemainingTime { get; set; }
+    public int CompletionTime { get; set; }
+
+
+    public Process(int process_id, int arrival_time, int burst_time)
+    {
+        Process_id = process_id;
+        ArrivalTime = arrival_time;
+        BurstTime = burst_time;
+        RemainingTime = burst_time;
+    }
+}
+
+class SRTF
+{
+    public static void schedule(List<Process> processList, int total_time)
+    {
+        int current_time = 0;
+        int completed = 0;
+        int total = processList.Count;
+
+        int throughput = 0;
+        List<Process> gantt_chart_process = new List<Process>();
+
+        while (completed < total)
+        {
+
+            var pending = processList
+                .Where(proc => proc.ArrivalTime <= current_time && proc.RemainingTime > 0)
+                .ToList();
+
+            if (pending.Count == 0)
+            {
+                current_time++;
+                continue;
+            }
+
+
+            var current_process = pending
+                .OrderBy(p => p.RemainingTime)
+                .ThenBy(p => p.ArrivalTime)
+                .First();
+
+
+            current_process.RemainingTime--;
+            gantt_chart_process.Add(current_process);
+
+            if (current_process.RemainingTime == 0)
+            {
+                current_process.CompletionTime = current_time + 1;
+                current_process.TurnaroundTime = current_process.CompletionTime - current_process.ArrivalTime;
+                current_process.WaitingTime = current_process.TurnaroundTime - current_process.BurstTime;
+                completed++;
+            }
+
+            current_time++;
+        }
+
+        displayGanttChart(gantt_chart_process);
+        displayResult(processList);
+
+
+        throughput = current_time / total;
+        Console.WriteLine("Throughput Process:" + throughput);
+        float cpu = ((float)total_time / (float)current_time) * 100;
+        Console.WriteLine("CPU Utilization  :" + cpu + "%");
+    }
+
+    private static void displayResult(List<Process> processList)
+    {
+        Console.WriteLine("\nProcess Information:");
+        Console.WriteLine("PID\tArrivalTime\tBurstTime\tCompletionTime\tTurnaround\tWaitingTime");
+        int avg_turn_time = 0;
+        int avg_wait_time = 0;
+        foreach (var proc in processList)
+        {
+            avg_turn_time += proc.TurnaroundTime;
+            avg_wait_time += proc.WaitingTime;
+            Console.WriteLine($"{proc.Process_id}\t{proc.ArrivalTime}\t\t{proc.BurstTime}\t\t{proc.CompletionTime}\t\t{proc.TurnaroundTime}\t\t{proc.WaitingTime}");
+        }
+
+        Console.WriteLine("Average Waiting Time:" + avg_wait_time / processList.Count);
+        Console.WriteLine("Average Turnaround Time:" + avg_turn_time / processList.Count);
+
+    }
+
+    private static void displayGanttChart(List<Process> processList)
+    {
+        Console.WriteLine("\nGantt Chart:");
+        foreach (var proc in processList)
+        {
+            Console.Write($"P{proc.Process_id} ");
+        }
+        Console.WriteLine();
+
+        for (int id = 0; id <= processList.Count; id++)
+        {
+            if (id < 10)
+                Console.Write(id == 0 ? "0  " : $"{id}  ");
+            else
+                Console.Write(id == 0 ? "0  " : $"{id} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int totalProcess = 0;
+        int all_process_time = 0;
+        char c = 'A';
+        List<Process> processes = new List<Process>();
+
+        Console.Write("Welcome To  SRTF Scheduling\n");
+        Console.Write("Please enter number of process: ");
+        var input = Console.ReadLine();
+        if (input != null)
+            totalProcess = int.Parse(input);
+
+
+        for (int i = 0; i < totalProcess; i++, c++)
+        {
+
+            Console.Write($"\nPlease enter arrival time and burst time of process {c}: ");
+            var @params = Console.ReadLine().Split();
+            int ArrivalTime = int.Parse(@params[0]);
+            int BurstTime = int.Parse(@params[1]);
+            Process process = new Process(i + 1, ArrivalTime, BurstTime);
+            processes.Add(process);
+            all_process_time += BurstTime;
+        }
+        SRTF.schedule(processes, all_process_time);
+    }
+}
